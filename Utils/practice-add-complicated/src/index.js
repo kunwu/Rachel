4d47bd4282aecb3fa787e26763b7dff4d2fe9126
kunwuorg <kunwu@kunwu.org>
2023-11-13 16:31:07 +0800

    add the main logic of the practice add complicated.

--- Utils/practice-add-complicated/src/index.js
+++ Utils/practice-add-complicated/src/index.js
@@ -8,8 +8,52 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+function getRandomIntWithDigits(d) {
+	let min = Math.pow(10, d - 1);
+	let max = Math.pow(10, d) - 1;
+	return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function generateAddQuestions(s, d, n) {
+	let numbers = [];
+	while (true) {
+		numbers = [];
+		let currentSum = 0;
+
+		for (let i = 0; i < n - 1; i++) {
+			numbers[i] = getRandomIntWithDigits(d);
+			currentSum += numbers[i];
+			if (currentSum > s) {
+				break;
+			}
+		}
+
+		if ((currentSum + Math.pow(10, d - 1) > s) || (currentSum + Math.pow(10, d) - 1 < s)) {
+			continue;
+		} else {
+			numbers[n - 1] = s - currentSum;
+		}
+
+		break;
+	}
+
+	// join the numbers with '+' sign, make cast if needed
+	numbers = numbers.join(' + ');
+	return numbers;
+}
+
 export default {
 	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
+		// get parameters if presented. if not presented, use default value
+		// s default to 1000
+		// d default to 3
+		// n default to 5
+		const params = new URL(request.url).searchParams;
+		const s = params.get('s') || 1000;
+		const d = params.get('d') || 3;
+		const n = params.get('n') || 5;
+
+		const questions = generateAddQuestions(s, d, n);
+		return new Response(questions);
 	},
 };
